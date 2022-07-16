8ebeff1aa4cfbda7f41b1a3e09ae0ff2032fe068
Quan Dac Tuan <dactuanbka@gmail.com>
2022-07-16 22:42:22 +0700

    test

--- TuanQD_Retrofit/app/src/main/java/com/example/retrofitassignment/network/client/ApiUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.retrofitassignment.network.client;
-import com.example.retrofitassignment.network.RetrofitAPIService;
-
-public class ApiUtils {
-    public static RetrofitAPIService getRetrofitAPIService() {
-        return RetrofitClient.getClient(RetrofitAPIService.BASE_URL)
-                .create(RetrofitAPIService.class);
-    }
-}
